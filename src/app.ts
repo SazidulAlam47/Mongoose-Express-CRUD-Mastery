f554d4f16b8693f54fb80e68cc62d594c9283da7
Sazidul Alam <sazidulalam20@gmail.com>
2025-01-20 19:40:17 +0600

    project is running

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Assignment-2: Hello World!");
+});
+
+export default app;
